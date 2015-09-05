ab38fa923ffbbb84c16507aba5e1dfdc198ed5d3
Oliver Hine <git@oliy.co.uk>
2015-09-06 00:53:25 +0100

    Adding Slacky logo

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ var button = buttons.ActionButton({
   id: "mozilla-link",
   label: "Visit Mozilla",
   icon: {
-    "16": "./icon-16.png",
-    "32": "./icon-32.png",
-    "64": "./icon-64.png"
+    "16": "./slacky-16.png",
+    "32": "./slacky-32.png",
+    "64": "./slacky-64.png"
   },
   onClick: openSlacky
 });
