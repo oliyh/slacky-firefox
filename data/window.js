4789091f932a28040a708034ab5fe1cecd376a7a
Oliver Hine <git@oliy.co.uk>
2015-09-06 13:35:36 +0100

    Letting you type /meme without forcing you to meme - only triggers dialogue when it's the last thing you typed

--- data/window.js
+++ data/window.js
@@ -1,9 +1,10 @@
-memePattern = new RegExp('\/meme', 'i');
+memePattern = new RegExp('\/meme$', 'i');
 
 function attachDomEventListeners() {
    $('textarea, input').keyup(function(event) {
       var userText = $(event.target).val();
-      if (memePattern.test(userText)) {
+      var textJustTyped = userText.substring(0, doGetCaretPosition(event.target));
+      if (memePattern.test(textJustTyped)) {
          self.port.emit('memeDetected', $(event.target).attr('id'));
       }
    });
