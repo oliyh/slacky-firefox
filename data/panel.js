d9c4acce0d4b521a4a9576b2565e5ffd57150a7c
Oliver Hine <git@oliy.co.uk>
2015-10-30 20:45:34 +0000

    Trying to solve cross origin problem with copying data

--- data/panel.js
+++ data/panel.js
@@ -17,8 +17,27 @@ function init() {
    });
 
    $('#copy-meme-data').click(function (e) {
+      var img = new Image,
+      canvas = document.createElement("canvas"),
+      ctx = canvas.getContext("2d"),
+      src = "http://i.memecaptain.com/gend_images/J1rjYA.jpg"; // insert image url here
+
+      img.crossOrigin = "Anonymous";
+
+      img.onload = function() {
+         canvas.width = img.width;
+         canvas.height = img.height;
+         ctx.drawImage( img, 0, 0 );
+         localStorage.setItem( "savedImageData", canvas.toDataURL("image/png") );
+      }
+      img.src = src;
+      // make sure the load event fires for cached images too
+      if ( img.complete || img.complete === undefined ) {
+         img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+         img.src = src;
+      }
+      /**
       var img = document.getElementById('meme');
-      img.crossOrigin = 'Anonymous';
       var canvas = document.createElement('CANVAS');
       var ctx = canvas.getContext('2d');
       canvas.height = img.height;
@@ -27,14 +46,15 @@ function init() {
       var dataURL = canvas.toDataURL('image/png');
       self.port.emit('copyToClipboard', dataURL);
       canvas = null;
-
+      */
    });
 
 
    self.port.on('panelOpened', function(t) {
       target = t;
-      $('#meme').attr('src', 'loading.gif');
+      $('#meme').attr('src', 'http://i.memecaptain.com/gend_images/J1rjYA.jpg').show();
       $('#error').text('').hide();
+      $('#meme-controls').show();
       $('#meme-input').val('').focus();
    });
 
@@ -46,6 +66,7 @@ function init() {
 
    self.port.on('badMemeRequest', function(helpText) {
       $('#meme').hide();
+      $('#meme-controls').hide();
       $('#error')
          .html(helpText.replace(/\n/g, '<br/>'))
          .show();
@@ -53,6 +74,7 @@ function init() {
 
    self.port.on('memeGenerationFailed', function(error) {
       $('#meme').hide();
+      $('#meme-controls').hide();
       $('#error')
          .html(error.replace(/\n/g, '<br/>'))
          .show();
