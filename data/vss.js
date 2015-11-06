2658a023526d62220029212e92d06ece19971009
Oliver Hine <git@oliy.co.uk>
2015-11-06 09:56:27 +0000

    Simplifying VSS

--- data/vss.js
+++ data/vss.js
@@ -5,36 +5,37 @@ jQuery(document).ready(function ($) {
    var sliderUlWidth = slideCount * slideWidth;
 
    $('#slider').css({ width: slideWidth, height: slideHeight });
+   $('#slider ul').css({ width: sliderUlWidth});
 
-   $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+   function toggleControls() {
+      if ($('#slider ul').position().left == 0) {
+         $('a.control_prev').hide();
+      } else {
+         $('a.control_prev').show();
+      }
 
-   $('#slider ul li:last-child').prependTo('#slider ul');
+      if ($('#slider ul').position().left == ((-1 * sliderUlWidth) + slideWidth)) {
+         $('a.control_next').hide();
+      } else {
+         $('a.control_next').show();
+      }
+   }
 
    function moveLeft() {
       $('#slider ul').animate({
-         left: + slideWidth
-      }, 200, function () {
-         $('#slider ul li:last-child').prependTo('#slider ul');
-         $('#slider ul').css('left', '');
-      });
+         left: $('#slider ul').position().left + slideWidth
+      }, 200, toggleControls);
    };
 
    function moveRight() {
       $('#slider ul').animate({
-         left: - slideWidth
-      }, 200, function () {
-         $('#slider ul li:first-child').appendTo('#slider ul');
-         $('#slider ul').css('left', '');
-      });
+         left: $('#slider ul').position().left - slideWidth
+      }, 200, toggleControls);
    };
 
-   $('a.control_prev').click(function () {
-      moveLeft();
-   });
-
-   $('a.control_next').click(function () {
-      moveRight();
-   });
+   $('a.control_prev').click(moveLeft);
+   $('a.control_next').click(moveRight);
+   toggleControls();
 
 });
 
@@ -42,11 +43,8 @@ function addSlide(slide) {
    $(slide).prependTo('#slider ul');
 
    var slideCount = $('#slider ul li').length;
-   var slideWidth = $('#slider ul li').width();
-   var slideHeight = $('#slider ul li').height();
    var sliderUlWidth = slideCount * slideWidth;
 
-   $('#slider').css({ width: slideWidth, height: slideHeight });
-   $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+   $('#slider ul').css({width: sliderUlWidth});
    $('#slider ul').css('left', '');
 }
